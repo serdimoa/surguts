5213182cda002906bc5826cf0b834ce406f34c93
Дмитрий Кравцов < >
2016-06-16 19:00:17 +0500

    first commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const connect = require('gulp-connect');
+
+gulp.task('connect', () => {
+  connect.server({
+    livereload: true,
+    root: './',
+    debug: true,
+  });
+});
+
+gulp.task('sass', () => {
+  gulp.src('./style/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./style/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('html', () => {
+  gulp.src('index.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watchhtml', () => {
+  gulp.watch('index.html', ['html']);
+});
+
+gulp.task('watch', () => {
+  gulp.watch('./style/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['connect', 'watch', 'watchhtml']);
